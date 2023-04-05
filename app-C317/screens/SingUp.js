3aa945ae088e38d069ad8af24de9d62cb37dd359
Maira <mairaalves@gec.inatel.br>
2023-04-04 22:39:08 -0300

    Regex email, senha e cpf

--- app-C317/screens/SingUp.js
+++ app-C317/screens/SingUp.js
@@ -6,12 +6,35 @@ import { Button, Input, Text } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { FontAwesome5 } from '@expo/vector-icons';
 import SignUpStyle from '../styles/SignUpStyle';
+import { validEmail, validPassword } from "../utils/regex";
 
 export default function SingUp({navigation}) {
   const [name, setName] = useState(null)
-  const [email, setEmail] = useState(null)
-  const [password, setPassword] = useState(null)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const [cpf, setCpf] = useState(null)
+
+  const [inputEmailErr, setInputEmailErr] = useState(false);
+  const [inputPassordErr, setInputPasswordErr] = useState(false);
+  const [inputCpfErr, setInputCpfErr] = useState(false);
+
+  const validate = () => {
+    if (!validEmail.test(email)) {
+      setInputEmailErr(true);
+    } else {
+      setInputEmailErr(false);
+      }
+    if (!validPassword.test(password)) {
+      setInputPasswordErr(true);
+    } else {
+      setInputPasswordErr(false);
+      }
+    if (!validCpf.test(cpf)) {
+      setInputCpfErr(true);
+    } else {
+      setInputCpfErr(false);
+      }  
+  };
 
   const confirmation = () => {
     navigation.navigate("Confirmation")
@@ -40,23 +63,29 @@ export default function SingUp({navigation}) {
       <View style={SignUpStyle.inputView}>
         <Input
           placeholder = "Nome completo" 
-          rightIcon = {{ type: 'font-awesome', name: 'eye' }} 
+          rightIcon = {{ type: 'font-awesome', name: 'eye' }}
+          value = {name} 
           onChangeText = {value => setName(value)} 
         />
 
         <Input
           placeholder = "E-mail" 
-          rightIcon = {{ type: 'font-awesome', name: 'check' }} 
+          rightIcon = {{ type: 'font-awesome', name: 'check' }}
+          value = {email}  
           onChangeText = {value => setEmail(value)} 
           keyboardType="email-address"
         />
+        {inputEmailErr && <Text>Por favor digete um email valido!</Text>}
 
         <View style={SignUpStyle.area}>
           <TextInput
             style={SignUpStyle.inputPassword}
             placeholder="Senha"
             secureTextEntry = {password}
+            value = {password}  
+            onChangeText = {value => setPassword(value)} 
           />
+          {inputPassordErr && <Text>Por favor digite uma senha mais segura!</Text>}
 
           <TouchableOpacity onPress={() => setPassword(!password)}>
             {password == true ?
@@ -69,18 +98,22 @@ export default function SingUp({navigation}) {
 
         <Input
           placeholder = "CPF" 
-          rightIcon = {{ type: 'font-awesome', name: 'check' }} 
+          rightIcon = {{ type: 'font-awesome', name: 'check' }}
+          value = {cpf}   
           onChangeText = {value => setCpf(value)} 
           secureTextEntry={ true }
           /* Poderia ter usado o keyboardType="number-pad" e dessa forma somente numeros seriam aceitos*/
           returnKeyType="done"
         />
+        {inputCpfErr && <Text>Por favor digete um cpf valido!</Text>}
       </View>
 
       <TouchableOpacity
         style={SignUpStyle.buttonSingIn} 
         activeOpacity={0.5}
-        onPress={() => confirmation()}
+        onClick={validate}
+        /*onPress={() => confirmation()}*/ /* verificar qual seria o + correto colocar e como incluir a lógica*/
+        
       >
         <Text style={SignUpStyle.buttonTextStyle}> 
           Cadastrar 
